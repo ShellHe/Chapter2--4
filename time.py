6127bfc6ef7d765c7fcc63640bb59b6f4225e9ee
Shell He <she2019@kewforest.org>
2017-09-19 19:59:43 -0400

    time
    time

--- time.py
+++ time.py
@@ -1,13 +1,13 @@
 now = float(input("what is the time now? "))
-wait = float(input("how many hours to wait? "))
+wait = float(input("how many hours to wait? "))
 
-totalhours = now + wait
+totalhours = now + wait
 
-alarm = total hours  % 24
+alarm = totalhours % 24
 
 if alarm > 12:
-    print(str(alarm) + "pm")
-else alarm < 12:
-    print(str(alarm) + "am")
+    print(str(alarm)+"pm")
+else:
+    print(str(alarm) + "am")
 
 print(alarm)
