64de6f34e155edb7c34c4921c8fe1d685eb9011a
Shell He <she2019@kewforest.org>
2017-09-20 09:34:55 -0400

    cool
    cool

---
+++ chapter3/chapter3 1.py
@@ -0,0 +1,2 @@
+for i in range(4000):
+    print("We like python's turtle!")
---
+++ turtle.py
@@ -0,0 +1,16 @@
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+wn.title("Hello, Alex!")
+alex = turtle.Turtle()
+alex.color("blue")
+alex.pensize(3)
+colors = ["red","orange", "blue", "SeaGrenn3"]
+for c in colors:
+       alex.color(c
+        for i in range(12):
+            for i in range(4):
+                alex.forward(50)
+                alex.left(90)
+            alex.left(30)
+        alex.left(10)
