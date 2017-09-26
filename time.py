ff4e39a9fd1c69a317edd7dad4726aeeb45d2cb6
Shell He <she2019@kewforest.org>
2017-09-26 12:04:33 -0400

    FIxed bug

--- time.py
+++ time.py
@@ -1,13 +1,18 @@
 now = float(input("what is the time now? "))
-wait = float(input("how many hours to wait? "))
+wait = float(input("how many hours to wait? "))
 
-totalhours = now + wait
 
-alarm = totalhours % 24
+totalhours = now + wait
+
+
+alarm = totalhours % 24
+
 
 if alarm > 12:
+    alarm = alarm % 12
     print(str(alarm)+"pm")
 else:
-    print(str(alarm) + "am")
+    print(str(alarm) + "am")
+
 
 print(alarm)
