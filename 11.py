6607c900c03ebf9b45e93323fa52fec480f38c0f
Shell He <she2019@kewforest.org>
2017-09-26 17:26:50 -0400

    hw

---
+++ 11.py
@@ -0,0 +1,10 @@
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("seashell")
+wn.title("Hello, Alex!")
+tess = turtle.Turtle()
+tess.pensize(2)
+for i in range(5):
+    tess.forward(100)
+    tess.right(145)
+tess.hideturtle()
---
+++ 8.py
@@ -0,0 +1,8 @@
+import turtle
+wn = turtle.Screen()
+allie = turtle.Turtle()
+
+for xs in [160, -43, 270, -97, -43, 200, -940, 17, -86]:
+    allie.forward(100)
+    allie.left(xs)
+print (allie.heading())
