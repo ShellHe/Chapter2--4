74c0231db1f6797e2f6c1cc163348a7c8a83f299
Shell He <she2019@kewforest.org>
2017-09-26 12:18:50 -0400

    hw

---
+++ 6d.py
@@ -0,0 +1,8 @@
+import turtle
+wn = turtle.Screen()
+allie = turtle.Turtle()
+
+sides = int(input("hwo many sides should I draw? "))
+for i in range(sides):
+    allie.forward(150)
+    allie.left(360/sides)
