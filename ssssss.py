9fffed17f07d721d4790fcff55ecf7caec7e4656
Shell He <she2019@kewforest.org>
2017-09-30 20:39:34 -0400

    hw

---
+++ ssssss.py
@@ -0,0 +1,21 @@
+import turtle
+
+def draw_squares(tess, size):
+    for i in range(size):
+        draw_multicolor_square(tess, size)
+        size = size + 10
+        tess.forward(10)
+        tess.right(18)   
+def draw_multicolor_square(t, sz):
+    """Make turtle t draw a multi-color square of sz."""
+    for i in ["red", "purple", "hotpink", "blue"]:
+#        t.color(i)
+        t.forward(sz)
+        t.right(90)
+        wn = turtle.Screen()
+        tess.pensize(3)
+
+size = 20
+tess = turtle.Turtle()
+draw_squares(tess, size)
+wn.mainloop()
---
+++ wison.py
@@ -0,0 +1,35 @@
+import turtle
+
+def drawsquare(turt,size):
+    for i in range(4):
+        turt.fd(size)
+        turt.left(90)
+
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+wn.title("turtles meet a function")
+
+alex = turtle.Turtle()
+wison = turtle.Turtle()
+wison.color("seashell")
+wison.setposition(-100,-100)
+size = 100
+for i in range(4):
+    alex.forward(size)
+    alex.left(90)
+
+alli = turtle.Turtle()
+alli.color("hotpink")
+alli.penup()
+alli.setpos(-100,-100)
+drawsquare(wison,100)
+drawsquare(alex, 200)
+
+shell = turtle.Turtle()
+shell.color("hotpink")
+shell.penup()
+shell.setpos(-100,-100)
+shell.pendown
+
+turts = [alex,shell.wison]
+for i in turts: drasquare(1,100)
