7b0e5f7f060cfa6dda34fe88e5a73f3b81237178
Shell He <she2019@kewforest.org>
2017-10-04 19:51:30 -0400

    mmm

---
+++ chapter4/4.4.py
@@ -0,0 +1,15 @@
+import turtle
+wn = turtle.Screen()
+tess = turtle.Turtle()
+wn.bgcolor("lightgreen")
+
+def make_pattern(turt,size):
+    for i in range(num):
+        
+        for i in range(4):
+            turt.lt(90)
+        turt.lf(angle)
+        
+make_pattern(tess,100)
+        
+                 
