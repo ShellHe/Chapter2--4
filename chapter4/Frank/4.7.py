8daae6cba165d0b4071f171eb069f8ff424daa06
Shell He <she2019@kewforest.org>
2017-10-18 14:39:23 -0400

    No issue
    Good Job!

---
+++ chapter4/Frank/4.7.py
@@ -0,0 +1,6 @@
+def sum_to(n):
+    first=0
+    for i in range(n):
+        first=first+i
+    return(sum([first,n]))
+print(sum_to(10))
---
+++ chapter4/Frank/4.9.py
@@ -0,0 +1,9 @@
+import turtle
+wn = turtle.Screen()
+frank = turtle. Turtle ()
+def draw_a_star(frank,length):
+    for i in range (5):
+        frank.forward(length)
+        frank. right(144)
+frank.hideturtle()
+draw_a_star(frank,100)
